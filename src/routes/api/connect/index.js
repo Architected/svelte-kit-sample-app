afe57e59d0b9b0184124578eafa48c22b278d2f6
Christian Farinella <christian@architected.net>
2022-01-25 07:29:49 +1100

    Fix impact of Breaking: use Request and Response objects in endpoints and hooks #3384

--- src/routes/api/connect/index.js
+++ src/routes/api/connect/index.js
@@ -1,18 +1,23 @@
 import { unexpectedError } from 'architected-client/helper/unexpectedError.js';
 import { connectClient } from '../../../service/setup.js';
 import dotenv from 'dotenv';
-export async function post(request) {
+export async function post({ request }) {
 	try {
 		dotenv.config();
 
+		console.log('connectClient.resolve');
+		const body = await request.json();
+
 		console.log('connectClient.authorize:connectInit:start');
-		const authorizeResponse = await connectClient.authorize(request.body);
+		const { data } = await connectClient.authorize(body);
+
 		console.log('connectClient.authorize:finish');
 
-		return {
+		const returnData = {
 			status: 200,
-			body: authorizeResponse != null ? authorizeResponse.data : null
+			body: data
 		};
+		return returnData;
 	} catch (err) {
 		console.log('error calling connectClient.authorize');
 		return {
