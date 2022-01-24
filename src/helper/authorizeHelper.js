ba599f7d6bc21ac2a203c4f772196a3bd92e3bfb
Christian Farinella <christian@architected.net>
2022-01-24 18:33:00 +1100

    Additional logging

--- src/helper/authorizeHelper.js
+++ src/helper/authorizeHelper.js
@@ -10,7 +10,7 @@ const startAuthorize = async (codeVerifier, clientDetails) => {
 	};
 
 	let response;
-
+	console.log('startAuthorize:start');
 	try {
 		response = await axios.post('/api/connect', request, {
 			timeout: 30000,
@@ -19,24 +19,25 @@ const startAuthorize = async (codeVerifier, clientDetails) => {
 			}
 		});
 	} catch (error) {
+		console.log('startAuthorize:error');
 		if (error.response) {
 			// The request was made and the server responded with a status code
 			// that falls out of the range of 2xx
-			console.log(error.response.data);
-			console.log(error.response.status);
-			console.log(error.response.headers);
+			console.log('error.response.data:' + JSON.stringify(error.response.data));
+			console.log('error.response.status:' + JSON.stringify(error.response.status));
+			console.log('error.response.headers:' + JSON.stringify(error.response.headers));
 		} else if (error.request) {
 			// The request was made but no response was received
 			// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
 			// http.ClientRequest in node.js
-			console.log(error.request);
+			console.log('error.request:' + JSON.stringify(error.request));
 		} else {
 			// Something happened in setting up the request that triggered an Error
-			console.log('Error', error.message);
+			console.log('Error' + error.message);
 		}
-		console.log(error.config);
+		console.log('error.config' + JSON.stringify(error.config));
 	}
-
+	console.log('startAuthorize:finish');
 	return response;
 };
 
